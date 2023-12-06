ca0414848a322f5321d440a762c749b262968987
HAN WU <whxhjs@gmail.com>
2023-12-06 19:34:06 +0800

    Modify: action space: MultiDiscrete -> Tuple w/ Several MultiDiscrete

--- custom_env/util/gen_action_sapce.py
+++ custom_env/util/gen_action_sapce.py
@@ -9,7 +9,7 @@ def gen_action_space(agent_assign_yaml_path):
     Parameters:
         agent_assign.yaml: the file that defines the action space including agent name and corresponding device name.
     Returns:
-        MultiDiscrete: the action space for the multi-agent environment.
+        Tuple w/ Several MultiDiscrete: the action space for the multi-agent environment.
     """
 
     # Define operation:
@@ -22,9 +22,19 @@ def gen_action_space(agent_assign_yaml_path):
         agent_assign = yaml.safe_load(file)
 
     action_space_dict = {}
-    for group_name, element in agent_assign.items():
-        action_space_dict[group_name] = gymnasium.spaces.MultiDiscrete([operation_number] * len(element))
+    # for group_name, element in agent_assign.items():
+    #     action_space_dict[group_name] = gymnasium.spaces.MultiDiscrete([operation_number] * len(element))
 
+    # For different type device, their action space is different. e.g., MOSFET -> MultiDiscrete([3]*3)
+    for group_name, device_list in agent_assign.items():
+        space_list = []
+        for device in device_list:
+            if device.startswith('M'):
+                space = gymnasium.spaces.MultiDiscrete([operation_number] * 3)
+            else:
+                space = gymnasium.spaces.MultiDiscrete([operation_number])
+            space_list.append(space)
+        action_space_dict[group_name] = gymnasium.spaces.Tuple(space_list)
     return action_space_dict
 
 
@@ -32,3 +42,9 @@ def gen_action_space(agent_assign_yaml_path):
 agent_assign_yaml_path = "../config/agent_assign.yaml"
 action_space_dict = gen_action_space(agent_assign_yaml_path)
 print(action_space_dict)
+
+# Output {'Group_1': Tuple(MultiDiscrete([3 3 3]), MultiDiscrete([3 3 3]), MultiDiscrete([3 3 3]), MultiDiscrete([3 3
+# 3]), MultiDiscrete([3 3 3]), MultiDiscrete([3 3 3]), MultiDiscrete([3 3 3]), MultiDiscrete([3 3 3])), 'Group_2':
+# Tuple(MultiDiscrete([3 3 3]), MultiDiscrete([3 3 3])), 'Group_3': Tuple(MultiDiscrete([3 3 3]), MultiDiscrete([3 3
+# 3]), MultiDiscrete([3 3 3]), MultiDiscrete([3 3 3]), MultiDiscrete([3 3 3]), MultiDiscrete([3 3 3])), 'Group_4':
+# Tuple(MultiDiscrete([3]))}
