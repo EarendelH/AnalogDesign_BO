2de379f17cc0f2466af8a0f6c3df4b685b578660
HAN WU <whxhjs@gmail.com>
2023-12-20 17:02:11 +0800

    Update ref

--- custom_env/env_test.py
+++ custom_env/env_test.py
@@ -2,5 +2,5 @@ from AnalogDesignAutoEnv import AnalogDesignEnv
 from pettingzoo.test import parallel_api_test
 
 if __name__ == "__main__":
-    env = AnalogDesignEnv(generalize=False, path='/Users/hanwu/ML/AnalogDesignAuto_MultiAgent/custom_env/sampled_specs/1.yaml')
+    env = AnalogDesignEnv(generalize=False, path='sampled_specs/1.yaml')
     parallel_api_test(env, num_cycles=1000)
