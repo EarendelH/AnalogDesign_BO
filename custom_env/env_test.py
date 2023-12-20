e40fe27037ea3924adb0fa39308c10074e5133fe
HAN WU <whxhjs@gmail.com>
2023-12-20 18:49:01 +0800

    update static value

--- custom_env/env_test.py
+++ custom_env/env_test.py
@@ -3,4 +3,4 @@ from pettingzoo.test import parallel_api_test
 
 if __name__ == "__main__":
     env = AnalogDesignEnv(generalize=False, path='sampled_specs/1.yaml')
-    parallel_api_test(env, num_cycles=1000)
+    parallel_api_test(env, num_cycles=5)
