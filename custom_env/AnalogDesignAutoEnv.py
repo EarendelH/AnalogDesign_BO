913e3b1315d72d781227c4c23da74880fd6c6612
HAN WU <whxhjs@gmail.com>
2023-12-20 17:14:39 +0800

    Update ref

--- custom_env/AnalogDesignAutoEnv.py
+++ custom_env/AnalogDesignAutoEnv.py
@@ -82,8 +82,8 @@ class AnalogDesignEnv(ParallelEnv):
                 unassigned_netlist_file_path = os.path.join(self.unassigned_netlist_dir, unassigned_netlist_file)
                 assigned_netlist_file_path = os.path.join(working_dir, assigned_netlist_file)
                 assigned_yaml_file_path = os.path.join(working_dir, "assigned.yaml")
-                assign_param2netlist(unassigned_netlist_file_path, assigned_netlist_file_path, assigned_yaml_file_path,
-                                     init_param)
+                assign_param2netlist(init_param, unassigned_netlist_file_path, assigned_netlist_file_path,
+                                     assigned_yaml_file_path)
 
         # Run spectre simulation
         with open(self.sim_config, 'r') as file:
