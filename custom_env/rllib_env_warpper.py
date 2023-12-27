c31efd0954010fa9c92b17fb3c75993bd5cd209c
HAN WU <whxhjs@gmail.com>
2023-12-27 13:26:22 +0800

    add env description

--- custom_env/rllib_env_warpper.py
+++ custom_env/rllib_env_warpper.py
@@ -233,7 +233,7 @@ if __name__ == "__main__":
     config = (
         get_trainable_cls(args.run)
         .get_default_config()
-        .environment(env=RllibAnalogDesignAutoEnv)
+        .environment(RllibAnalogDesignAutoEnv(generalize=True, path='sampled_specs'))
         .resources(
             # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
             num_gpus=int(os.environ.get("RLLIB_NUM_GPUS", "0")),
