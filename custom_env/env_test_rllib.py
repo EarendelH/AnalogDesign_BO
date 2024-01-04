5b902667e88c7360468b841b9557759639836cb0
HAN WU <whxhjs@gmail.com>
2024-01-04 21:06:22 +0800

    Update normlization method

--- custom_env/env_test_rllib.py
+++ custom_env/env_test_rllib.py
@@ -2,6 +2,6 @@ from ray.rllib.utils import check_env
 from rllib_env_warpper import RllibAnalogDesignAutoEnv
 
 if __name__ == "__main__":
-    env = RllibAnalogDesignAutoEnv(generalize=False, path='sampled_specs/1.yaml')
+    env = RllibAnalogDesignAutoEnv(generalize=True, path='sampled_specs')
     check_env(env)
     print("Test passed")
