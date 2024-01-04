a200f515e90e0103cdf8a6d20f3bd219fe4b0555
HAN WU <whxhjs@gmail.com>
2024-01-04 17:58:10 +0800

    Comment debug print

--- custom_env/util/normlization.py
+++ custom_env/util/normlization.py
@@ -13,11 +13,11 @@ def normalization(ideal_specs, cur_specs):
         norm_cur_specs_sub = {}
         for sub_key, value in sub_dict.items():
             cur_value = value
-            print(f"Debug, cur_value = {cur_value}")
+            # print(f"Debug, cur_value = {cur_value}")
             ideal_value = ideal_specs[sub_key]['value']
-            print(f"Debug, ideal_value = {ideal_value}")
+            # print(f"Debug, ideal_value = {ideal_value}")
             norm_value = (cur_value - ideal_value) / (cur_value + ideal_value)
-            print(f"Debug, norm_value = {norm_value}")
+            # print(f"Debug, norm_value = {norm_value}")
             norm_cur_specs_sub[sub_key] = norm_value
         norm_cur_specs[key] = norm_cur_specs_sub
 
