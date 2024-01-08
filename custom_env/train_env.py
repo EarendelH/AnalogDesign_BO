2a282488bc33a9684d83da290bf2314524ef740a
HAN WU <whxhjs@gmail.com>
2024-01-08 10:53:27 +0800

    debug

--- custom_env/train_env.py
+++ custom_env/train_env.py
@@ -75,7 +75,7 @@ if __name__ == "__main__":
         "PPO",
         name="PPO",
         stop={"timesteps_total": 5000},
-        checkpoint_freq=10,
-        local_dir="~/ray_results/" + env_name,
+        checkpoint_freq=5,
+        local_dir="/home/wuhan/ray_results/" + env_name,
         config=config.to_dict(),
     )
