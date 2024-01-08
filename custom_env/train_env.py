3c67af8717ccfd96953c4c225fcb7fed8f43bd44
HAN WU <whxhjs@gmail.com>
2024-01-08 10:58:11 +0800

    debug

--- custom_env/train_env.py
+++ custom_env/train_env.py
@@ -61,7 +61,7 @@ if __name__ == "__main__":
             sgd_minibatch_size=64,
             num_sgd_iter=10,
         )
-        .debugging(log_level="ERROR")
+        .debugging(log_level="DEBUG")
         .framework("torch")
         .resources(num_gpus=int(os.environ.get("RLLIB_NUM_GPUS", "0")))
         .multi_agent(
