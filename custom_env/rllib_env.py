42b0106140a445257c49deb0031ca5bd857f48d5
HAN WU <whxhjs@gmail.com>
2024-01-10 18:34:10 +0800

    Add print line

--- custom_env/rllib_env.py
+++ custom_env/rllib_env.py
@@ -267,4 +267,7 @@ class RllibAnalogDesignAutoEnv(MultiAgentEnv):
         terminated["__all__"] = len(self.terminateds) == len(self.agents)
         truncated["__all__"] = len(self.truncateds) == len(self.agents)
 
+        print(f"Step!!!terminated: {terminated} with step number: {self.step_num}")
+        print(f"Step!!!truncated: {truncated} with step number: {self.step_num}")
+
         return observations, rew, terminated, truncated, info
