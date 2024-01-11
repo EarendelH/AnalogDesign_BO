450d0b3cad3bf2aac9ca4ace841474706b2ba951
HAN WU <whxhjs@gmail.com>
2024-01-11 15:13:59 +0800

    Change save path

--- custom_env/train_env_gpu.py
+++ custom_env/train_env_gpu.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     config = (
         PPOConfig()
         .environment(env="AnalogDesignEnv_v0", clip_actions=True)
-        .rollouts(num_rollout_workers=20)
+        .rollouts(num_rollout_workers=36)
         .training(
             train_batch_size=512,
             lr=2e-4,
