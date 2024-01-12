4e7e6ee76e4ff4092c57a4d6ce082c21f3604802
HAN WU <whxhjs@gmail.com>
2024-01-12 16:15:27 +0800

    Add spectre config

--- custom_env/train_env_gpu.py
+++ custom_env/train_env_gpu.py
@@ -58,7 +58,7 @@ if __name__ == "__main__":
         "PPO",
         name="PPO",
         stop={"training_iteration": 500,
-              "episode_reward_mean": 100},
+              "episode_reward_mean": 50},
         checkpoint_freq=1,
         checkpoint_at_end=True,
         local_dir="/home/wuhan/ray_results/" + env_name,
