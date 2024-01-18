9d1510c63b5cda72f74d6f6a1252cbcc68c45501
HAN WU <whxhjs@gmail.com>
2024-01-18 13:21:42 +0800

    Update validate.py

--- custom_env/validate.py
+++ custom_env/validate.py
@@ -22,6 +22,7 @@ def run_evaluation(checkpoint_path, num_episodes=10):
 
     for episode in range(num_episodes):
         obs = env.reset()
+        print(f"Observation: {obs}")
         done = {"__all__": False}
         while not done["__all__"]:
             action_dict = {}
