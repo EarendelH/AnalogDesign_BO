fbfb0980130dbcbe363e06719a8b11297a5803e2
HAN WU <whxhjs@gmail.com>
2024-01-22 16:03:09 +0800

    Update export_onnx.py

--- custom_env/export_onnx.py
+++ custom_env/export_onnx.py
@@ -19,6 +19,26 @@ register_env("AnalogDesignEnv_v0", env_creator)
 ppo_config = (
     PPOConfig()
     .environment(env="AnalogDesignEnv_v0", clip_actions=True)
+    .rollouts(num_rollout_workers=36)
+    .training(
+        train_batch_size=512,
+        lr=2e-5,
+        gamma=0.99,
+        lambda_=0.9,
+        use_gae=True,
+        clip_param=0.4,
+        grad_clip=None,
+        entropy_coeff=0.1,
+        vf_loss_coeff=0.25,
+        sgd_minibatch_size=64,
+        num_sgd_iter=10,
+        model={
+            "fcnet_hiddens": [256, 256, 256, 256, 256],
+        }
+    )
+    .debugging(log_level="DEBUG")
+    .framework("torch")
+    .resources(num_gpus=int(os.environ.get("RLLIB_NUM_GPUS", "0")))
     .multi_agent(
         policies={"policy_1", "policy_2", "policy_3", "policy_4"},
         policy_mapping_fn=(lambda aid, episode, worker, **kw: f"policy_{aid[-1]}"),
