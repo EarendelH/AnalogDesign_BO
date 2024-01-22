32d81cf91b39e0d37b6fb66cec163a272410ea9d
HAN WU <whxhjs@gmail.com>
2024-01-22 14:49:28 +0800

    Update pickle_reader.py

--- custom_env/debug_util/pickle_reader.py
+++ custom_env/debug_util/pickle_reader.py
@@ -7,17 +7,4 @@ pickle_file = "../checkpoint_demo/checkpoint_000009/policies/policy_1/policy_sta
 with open(pickle_file, 'rb') as f:
     data = pickle.load(f)
 
-def convert_ndarray(data):
-    if isinstance(data, np.ndarray):
-        return data.tolist()
-    elif isinstance(data, dict):
-        return {k: convert_ndarray(v) for k, v in data.items()}
-    elif isinstance(data, list):
-        return [convert_ndarray(v) for v in data]
-    else:
-        return data
-
-converted_data = convert_ndarray(data)
-
-with open('policy_state.json', 'w') as f:
-    json.dump(data, f, indent=4)
+print(data)
