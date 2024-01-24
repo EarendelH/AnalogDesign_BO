fd527b93b71a0796b61063d066cd1ef7bab92837
HAN WU <whxhjs@gmail.com>
2024-01-24 21:25:45 +0800

    Absolute value for pwr

--- custom_env/train_env_gpu.py
+++ custom_env/train_env_gpu.py
@@ -57,9 +57,9 @@ if __name__ == "__main__":
     tune.run(
         "PPO",
         name="PPO",
-        stop={"training_iteration": 200},
-        checkpoint_freq=1,
+        stop={"training_iteration": 5000},
+        checkpoint_freq=10,
         checkpoint_at_end=True,
-        local_dir="/home/wuhan/ray_results/" + env_name,
+        local_dir="~/ray_results/" + env_name,
         config=config.to_dict(),
     )
