a6e6a33324df518a5f158f60b6f6ecd53322d3a3
HAN WU <whxhjs@gmail.com>
2024-01-29 22:24:54 +0800

    Update env_test_rllib.py for fitting new env input

--- custom_env/env_test_rllib.py
+++ custom_env/env_test_rllib.py
@@ -2,6 +2,7 @@ from ray.rllib.utils import check_env
 from rllib_env import RllibAnalogDesignAutoEnv
 
 if __name__ == "__main__":
-    env = RllibAnalogDesignAutoEnv(generalize=True, path='sampled_specs')
+    env = RllibAnalogDesignAutoEnv(action_mask=True, generalize=True, path='sampled_specs', sim_output=False,
+                                   init_method='random')
     check_env(env)
     print("Test passed")
