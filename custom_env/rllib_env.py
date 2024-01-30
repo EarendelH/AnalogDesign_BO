b35055c22ee95910a9836d4af8a63e8b589f96ba
HAN WU <whxhjs@gmail.com>
2024-01-30 16:54:00 +0800

    Update rllib_env.py: Add DC check in reset method.

--- custom_env/rllib_env.py
+++ custom_env/rllib_env.py
@@ -21,13 +21,15 @@ from util.update_obs_space import update_obs_space, flatten_observation
 from util.normlization import norm_ideal_spec, norm_sim_spec
 from util.util_func import retry_decorator
 from util.init_param import gen_init_param
+from util.extract_device_param_value import extract_operation_region
 
 
 from ray.rllib.env.multi_agent_env import MultiAgentEnv
 
 
 class RllibAnalogDesignAutoEnv(MultiAgentEnv):
-    def __init__(self, action_mask=True, generalize=True, path='', sim_output=False, init_method='file'):
+    def __init__(self, generalize=True, path='', sim_output=False, init_method='file', action_mask=True,
+                 init_dc_check=True):
 
         # Init values
         self.resetted = None
@@ -39,13 +41,16 @@ class RllibAnalogDesignAutoEnv(MultiAgentEnv):
         self.norm_ideal_specs = None
         self.ideal_specs = None
         self.cur_param = None
-        self.max_step = 512
+        self.max_step = 1024
 
         # Get absolute path
         self.current_path = os.getcwd()
 
         # Pass action mask flag
         self.action_mask = action_mask
+
+        # Pass initial dc check flag
+        self.init_dc_check = init_dc_check
 
         # Pass generalization flag
         self.generalize = generalize
@@ -76,8 +81,8 @@ class RllibAnalogDesignAutoEnv(MultiAgentEnv):
         self.param_range_config = os.path.join(self.current_path, self.param_range_config)
         self.sim_config = "config/simulation.yaml"
         self.sim_config = os.path.join(self.current_path, self.sim_config)
-        self.init_param = "config/init_param.yaml"
-        self.init_param = os.path.join(self.current_path, self.init_param)
+        self.predefined_init_param = "config/init_param.yaml"
+        self.predefined_init_param = os.path.join(self.current_path, self.predefined_init_param)
         self.device_mask_config = "config/device_mask.yaml"
         self.device_mask_config = os.path.join(self.current_path, self.device_mask_config)
 
@@ -124,13 +129,54 @@ class RllibAnalogDesignAutoEnv(MultiAgentEnv):
         print(f"Initialing!!!Ideal specs: {self.ideal_specs}")
 
         # Generate init param
-        init_param = gen_init_param(self.init_method, self.init_param, self.action_mask, self.device_mask_config,
-                                    self.param_space)
+        if self.init_method == 'random' and self.init_dc_check:
+            valid_init_param = False
+            init_step = 0
+            while not valid_init_param:
+                init_param = gen_init_param(self.init_method, self.predefined_init_param, self.action_mask,
+                                            self.device_mask_config, self.param_space)
+                # Check the operation region of transistors with the given init params
+                working_dir = create_work_dir(self.run_root_dir)
+                print(f"Initialing Checking!!!Working directory: {working_dir} with init step number: {init_step}")
+
+                # Update DC Netlist File for checking init operation region
+                unassigned_netlist_file = "DC_parameterized.scs"
+                assigned_netlist_file = "DC.scs"
+                unassigned_netlist_file_path = os.path.join(self.unassigned_netlist_dir, unassigned_netlist_file)
+                assigned_netlist_file_path = os.path.join(working_dir, assigned_netlist_file)
+                assigned_yaml_file_path = os.path.join(working_dir, "assigned.yaml")
+                assign_param2netlist(init_param, unassigned_netlist_file_path, assigned_netlist_file_path,
+                                     assigned_yaml_file_path)
+
+                with open(self.sim_config, 'r') as file:
+                    sim_config = yaml.safe_load(file)
+
+                dc_sim_config = [item for item in sim_config if item['simulation_name'] == 'DC']
+                _ = run_spectre_simulation(working_dir, dc_sim_config, self.sim_output_enable)
+                dc_result_path = os.path.join(working_dir, "DC.raw/dcOpInfo.info.encode")
+                operation_region_list = extract_operation_region(dc_result_path)
+                print(f"Initialing Checking!!!Operation region: {operation_region_list} with "
+                      f"init step number: {init_step}")
+                # 0 cut-off, 1 triode, 2 saturation, 3 subth, 4 breakdown
+                # Check whether all transistors are in saturation/sub-threshold/triode region
+                valid_init_param = any(item != 1 and item != 2 and item != 3 for item in operation_region_list)
+                init_step += 1
+                # Delete working temp directory, if it exists
+                try:
+                    if os.path.exists(working_dir):
+                        shutil.rmtree(working_dir)
+                except OSError as e:
+                    print(f"Warning!!!: {e.strerror}. Directory {working_dir} does not exist or cannot be removed.")
+                    pass
+        else:
+            init_param = gen_init_param(self.init_method, self.predefined_init_param, self.action_mask,
+                                        self.device_mask_config, self.param_space)
 
         # Generate working directory
         working_dir = create_work_dir(self.run_root_dir)
         print(f"Initialing!!!Working directory: {working_dir}")
 
+        # Update Netlist File
         for unassigned_netlist_file in os.listdir(self.unassigned_netlist_dir):
             if unassigned_netlist_file.endswith(".scs"):
                 assigned_netlist_file = unassigned_netlist_file.replace("_parameterized", "")
