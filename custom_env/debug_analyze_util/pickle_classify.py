9dac56ac0087e4d6ccd6c40e7bd257f7dc3db4fb
HAN WU <whxhjs@gmail.com>
2024-02-20 15:05:54 +0800

    Add pickle_classify.py

---
+++ custom_env/debug_analyze_util/pickle_classify.py
@@ -0,0 +1,29 @@
+import os
+import pickle
+import shutil
+
+
+def count_sim_params(pickle_file):
+    """Count the number of simulation parameters in a pickle file."""
+    with open(pickle_file, 'rb') as f:
+        data = pickle.load(f)
+    sim_results = data['initial_data']['sim_result']
+    return len(sim_results)
+
+
+def classify_pickle_files(source_dir):
+    """Classify pickle files by the number of simulation parameters."""
+    for filename in os.listdir(source_dir):
+        if filename.endswith('.pkl'):
+            pickle_path = os.path.join(source_dir, filename)
+            num_params = count_sim_params(pickle_path)
+            target_dir = os.path.join(source_dir, f'specs_{num_params}')
+            if not os.path.exists(target_dir):
+                os.makedirs(target_dir)
+            shutil.move(pickle_path, os.path.join(target_dir, filename))
+            print(f'Moved {filename} to {target_dir}')
+
+
+if __name__ == '__main__':
+    source_dir = 'path/to/your/pickle/files'
+    classify_pickle_files(source_dir)
