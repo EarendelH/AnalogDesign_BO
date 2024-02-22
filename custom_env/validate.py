bbef06a9f7d29a5b4ae72d99ab56808273f37b68
HAN WU <whxhjs@gmail.com>
2024-02-22 12:38:33 +0800

    Update validate.py, improve interaction

--- custom_env/validate.py
+++ custom_env/validate.py
@@ -1,38 +1,59 @@
 import logging
+import os
+
 import ray
 from ray.rllib.algorithms.algorithm import Algorithm
 from rllib_env_v3 import RllibAnalogDesignAutoEnv
 from ray.tune.registry import register_env
+from ray.util.multiprocessing import Pool
 
 
-def env_creator(env_config, path):
-    return RllibAnalogDesignAutoEnv(generalize=True, path=path, sim_output=False, init_method='random',
+def env_creator(env_config, validate_specs_path):
+    return RllibAnalogDesignAutoEnv(generalize=True, path=validate_specs_path, sim_output=False, init_method='random',
                                     action_mask=True, init_dc_check=True)
 
 
-def run_evaluation(checkpoint_file_path, path, num_episodes=10):
-    ray.init(logging_level=logging.WARNING)
-
+def evaluate_episode(checkpoint_file_path, path, episode_num):
     register_env("AnalogDesignEnv_v0", lambda env_config: env_creator(env_config, path))
     env = env_creator({}, path)
-
     agent = Algorithm.from_checkpoint(checkpoint_file_path)
+    obs, _ = env.reset()
+    done = {"__all__": False}
+    step = 0
+    count = 0
+    count_finished = 0
+    count_terminated = 0
+    while not done["__all__"]:
+        action_dict = {}
+        for agent_id, agent_obs in obs.items():
+            policy_id = f"policy_{agent_id[-1]}"
+            action = agent.compute_single_action(agent_obs, policy_id=policy_id)
+            action_dict[agent_id] = action
+        obs, rew, done, _, info = env.step(action_dict)
+        step += 1
+        if done["__all__"]:
+            count += 1
+            count_finished += 1
+            print(f"Total valid episodes: {count_finished}, finished episodes: {count_finished} "
+                  f"and terminated episodes: {count_terminated}")
+        if step > 1000:
+            print(f"Episode {episode_num} terminated due to exceeding max steps")
+            count += 1
+            count_terminated += 1
+            print(f"Total valid episodes: {count_finished}, finished episodes: {count_finished} "
+                  f"and terminated episodes: {count_terminated}")
+            break
+    return f"Episode {episode_num} finished after {step} steps"
 
-    for episode in range(num_episodes):
-        obs, _ = env.reset()
-        done = {"__all__": False}
-        step = 0
-        while not done["__all__"]:
-            action_dict = {}
-            for agent_id, agent_obs in obs.items():
-                policy_id = f"policy_{agent_id[-1]}"
-                action = agent.compute_single_action(agent_obs, policy_id=policy_id)
-                action_dict[agent_id] = action
-            obs, rew, done, _, info = env.step(action_dict)
-            step += 1
 
-        print(f"Episode {episode} finished after {step} steps")
-
+def run_evaluation_parallel(checkpoint_file_path, validate_specs_path, num_episodes, used_cpu):
+    ray.init(logging_level=logging.WARNING)
+    # Using a process pool to run evaluations in parallel
+    with Pool(processes=used_cpu) as pool:
+        results = pool.starmap(evaluate_episode, [(checkpoint_file_path, validate_specs_path, episode) for episode in
+                                                  range(num_episodes)])
+        for result in results:
+            print(result)
     ray.shutdown()
 
 
@@ -42,4 +63,6 @@ if __name__ == "__main__":
     path = input("Enter the folder path for validation: ").strip()
     num_episode = input("Enter the number of episodes: ").strip()
     num_episode = int(num_episode)
-    run_evaluation(checkpoint_path, path, num_episode)
+    num_cpu_available = os.cpu_count()
+    num_cpu = input(f"Enter the number of CPUs to use (Available num is {num_cpu_available}): ").strip()
+    run_evaluation_parallel(checkpoint_path, path, num_episode, num_cpu)
