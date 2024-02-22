4302313f4be8ad86e2e5edd2bc2ab01196ab58a3
HAN WU <whxhjs@gmail.com>
2024-02-22 13:15:20 +0800

    Update validate.py, improve interaction

--- custom_env/validate.py
+++ custom_env/validate.py
@@ -3,8 +3,6 @@ import ray
 from ray.rllib.algorithms.algorithm import Algorithm
 from rllib_env_v3 import RllibAnalogDesignAutoEnv
 from ray.tune.registry import register_env
-from concurrent.futures import ThreadPoolExecutor, as_completed
-import os
 
 
 def env_creator(env_config, validate_path):
@@ -12,51 +10,40 @@ def env_creator(env_config, validate_path):
                                     action_mask=True, init_dc_check=True)
 
 
-def evaluate_episode(checkpoint_file_path, validate_path, episode_num):
-    env = env_creator({}, validate_path)
-    agent = Algorithm.from_checkpoint(checkpoint_file_path)
-    obs, _ = env.reset()
-    done = {"__all__": False}
-    step = 0
-    while not done["__all__"]:
-        action_dict = {}
-        for agent_id, agent_obs in obs.items():
-            policy_id = f"policy_{agent_id[-1]}"
-            action = agent.compute_single_action(agent_obs, policy_id=policy_id)
-            action_dict[agent_id] = action
-        obs, rew, done, _, info = env.step(action_dict)
-        step += 1
-        if step > 1000:
-            return {'episode': episode_num, 'steps': step, 'passed': False}
-    return {'episode': episode_num, 'steps': step, 'passed': True}
-
-
-def run_evaluation_parallel(checkpoint_file_path, validate_path, num_episodes=10, util_cpu=4):
+def run_evaluation(checkpoint_file_path, path, num_episodes=10):
     ray.init(logging_level=logging.WARNING)
-    register_env("AnalogDesignEnv_v0", lambda env_config: env_creator(env_config, validate_path))
 
-    passed_count = 0
-    failed_count = 0
+    register_env("AnalogDesignEnv_v0", lambda env_config: env_creator(env_config, path))
+    env = env_creator({}, path)
 
-    with ThreadPoolExecutor(max_workers=util_cpu) as executor:
-        futures = [executor.submit(evaluate_episode, checkpoint_file_path, validate_path, episode) for episode
-                   in range(num_episodes)]
-        for future in as_completed(futures):
-            result = future.result()
-            if result['passed']:
-                passed_count += 1
-            else:
-                failed_count += 1
-            print(f"Total Completed: {passed_count + failed_count}, Passed: {passed_count}, Failed: {failed_count}")
+    agent = Algorithm.from_checkpoint(checkpoint_file_path)
+
+    for episode in range(num_episodes):
+        obs, _ = env.reset()
+        done = {"__all__": False}
+        step = 0
+        count_terminated = 0
+        count_finished = 0
+        while not done["__all__"]:
+            action_dict = {}
+            for agent_id, agent_obs in obs.items():
+                policy_id = f"policy_{agent_id[-1]}"
+                action = agent.compute_single_action(agent_obs, policy_id=policy_id)
+                action_dict[agent_id] = action
+            obs, rew, done, _, info = env.step(action_dict)
+            step += 1
+            if step > 1024:
+                print(f"Episode {episode} terminated due to exceeding the maximum number of steps")
+                count_terminated += 1
+                break
+            if done["__all__"]:
+                count_finished += 1
+                print(f"Episode {episode} finished after {step} steps")
+
+        print(f"Total episodes: {count_finished + count_terminated}, Finished: {count_finished}, "
+              f"Terminated: {count_terminated}")
 
     ray.shutdown()
-
-
-def get_user_thread_count():
-    max_workers = os.cpu_count()
-    print(f"Available threads: {max_workers}")
-    cpu_num = input("Enter the number of threads to use: ").strip()
-    return min(max_workers, int(cpu_num))
 
 
 if __name__ == "__main__":
@@ -65,5 +52,4 @@ if __name__ == "__main__":
     path = input("Enter the folder path for validation: ").strip()
     num_episode = input("Enter the number of episodes: ").strip()
     num_episode = int(num_episode)
-    num_workers = get_user_thread_count()
-    run_evaluation_parallel(checkpoint_path, path, num_episode, num_workers)
+    run_evaluation(checkpoint_path, path, num_episode)
