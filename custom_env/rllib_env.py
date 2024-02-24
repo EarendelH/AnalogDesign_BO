33a1c14fad1aab6cfa895dcf72cf712a8fe7f1c4
HAN WU <whxhjs@gmail.com>
2024-02-24 23:00:53 +0800

    Set root directory based on home directory Avoid run folder placing in /tmp directory when running on cluster

--- custom_env/rllib_env.py
+++ custom_env/rllib_env.py
@@ -81,11 +81,15 @@ class RllibAnalogDesignAutoEnv(MultiAgentEnv):
         # Get run file directory
         file_dir = os.path.dirname(os.path.abspath(__file__))
 
+        # Get home directory
+        self.home_dir = os.path.expanduser("~")
+
         # Set root directory based on home directory
         # Avoid run folder placing in /tmp directory when running on cluster
         # ~/AnalogDesignAuto/AnalogDesignAuto_MultiAgent/custom_env/ + self.run_root_dir
         self.run_root_dir = run_folder_name
-        self.run_root_dir = os.path.join(file_dir, self.run_root_dir)
+        self.run_root_dir = os.path.join(self.home_dir, "AnalogDesignAuto/AnalogDesignAuto_MultiAgent/custom_env",
+                                         self.run_root_dir)
         if not os.path.exists(self.run_root_dir):
             raise ValueError(f"Root directory {self.run_root_dir} not found.")
 
