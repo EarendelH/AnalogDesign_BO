9ba44b2dced65a9f9d19aa501889ad5b529d1be2
HAN WU <whxhjs@gmail.com>
2024-02-27 17:21:37 +0800

    Update rllib_env.py for improving

--- custom_env/env_test_rllib_v3.py
+++
@@ -1,9 +0,0 @@
-from ray.rllib.utils import check_env
-from rllib_env_v3 import RllibAnalogDesignAutoEnv
-
-if __name__ == "__main__":
-    env = RllibAnalogDesignAutoEnv(generalize=True, path='sampled_specs_v3', sim_output=False, init_method='random',
-                                   action_mask=True, init_dc_check=True)
-
-    check_env(env)
-    print("Test passed")
