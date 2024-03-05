d2c2a54281335b2fa49219b62d569ab475f1216e
HAN WU <whxhjs@gmail.com>
2024-03-05 15:14:56 +0800

    Add validation specs v4

--- custom_env/validate.py
+++ custom_env/validate.py
@@ -1,23 +1,24 @@
 import logging
 import ray
+import os
 
 from ray.rllib.algorithms.algorithm import Algorithm
 from rllib_env import RllibAnalogDesignAutoEnv
 from ray.tune.registry import register_env
 
 
-def env_creator(env_config, validate_dir_name, config_dir_name):
+def env_creator(env_config, validate_dir_name, config_dir_name, run_dir_name):
     return RllibAnalogDesignAutoEnv(generalize=True, specs_folder_name=validate_dir_name,
-                                    config_folder_name=config_dir_name, run_folder_name='run_test_validate',
+                                    config_folder_name=config_dir_name, run_folder_name=run_dir_name,
                                     sim_output=False, init_method='file', action_mask=True, dc_check=True)
 
 
-def run_evaluation(checkpoint_file_path, validate_file_path, config_dir_name, num_episodes):
+def run_evaluation(checkpoint_file_path, validate_file_path, config_dir_name, run_dir_name, num_episodes):
     ray.init(logging_level=logging.WARNING)
 
     register_env("AnalogDesignEnv_v0", lambda env_config: env_creator(env_config, validate_file_path,
                                                                       config_dir_name))
-    env = env_creator({}, validate_file_path, config_dir_name)
+    env = env_creator({}, validate_file_path, config_dir_name, run_dir_name)
 
     agent = Algorithm.from_checkpoint(checkpoint_file_path)
 
@@ -51,10 +52,17 @@ def run_evaluation(checkpoint_file_path, validate_file_path, config_dir_name, nu
 
 if __name__ == "__main__":
     print("Init Validation")
+    # Get the script path
+    running_script_path = os.path.abspath(__file__)
     checkpoint_path = input("Enter the checkpoint path: ").strip()
     validate_file_folder = input("Enter the validation folder path: ").strip()
     config_folder_name = input("Enter the config folder name: ").strip()
+    run_dir_name = input("Enter the run folder name: ").strip()
+    run_dir_full_path = os.path.join(os.path.dirname(running_script_path), run_dir_name)
+    if not os.path.exists(run_dir_full_path):
+        os.makedirs(run_dir_full_path)
+        print(f"Directory {run_dir_full_path} created")
     num_episode = input("Enter the number of episodes: ").strip()
     num_episode = int(num_episode)
 
-    run_evaluation(checkpoint_path, validate_file_folder, config_folder_name, num_episode)
+    run_evaluation(checkpoint_path, validate_file_folder, config_folder_name, run_dir_name, num_episode)
