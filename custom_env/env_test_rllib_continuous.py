2a4ae5612178732c68e1f7c56e802a7f252310f6
HAN WU <whxhjs@gmail.com>
2024-03-26 00:08:57 +0800

    continuous action space

--- custom_env/env_test_rllib_continuous.py
+++ custom_env/env_test_rllib_continuous.py
@@ -2,8 +2,8 @@ from ray.rllib.utils import check_env
 from rllib_env_continous import RllibAnalogDesignAutoEnv
 
 if __name__ == "__main__":
-    env = RllibAnalogDesignAutoEnv(generalize=True, specs_folder_name='sampled_specs_simple',
-                                   config_folder_name='config_simple', run_folder_name='run_test_simple',
+    env = RllibAnalogDesignAutoEnv(generalize=True, specs_folder_name='sampled_specs',
+                                   config_folder_name='config', run_folder_name='run_test',
                                    sim_output=False, init_method='file', action_mask=True, dc_check=True)
 
     check_env(env)
