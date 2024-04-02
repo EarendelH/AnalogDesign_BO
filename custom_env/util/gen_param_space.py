2950394cdc01574b250a66cb87cfbddb54e6f05f
HAN WU <whxhjs@gmail.com>
2024-04-02 22:23:19 +0800

    Debug

--- custom_env/util/gen_param_space.py
+++ custom_env/util/gen_param_space.py
@@ -41,7 +41,7 @@ def gen_param_space(param_range_dict):
             values = [str(int(value)) for value in values]
         else:
             # Add the magnitude character back (if any)
-            values = [f"{value}{magnitude}" if magnitude else str(value) for value in values]
+            values = [f"{round(value, 1)}{magnitude}" if magnitude else str(value) for value in values]
 
         return values
 
