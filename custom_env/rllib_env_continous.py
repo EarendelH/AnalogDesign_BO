207fa06ca670e171530b7ce6f34c979a444ab515
HAN WU <whxhjs@gmail.com>
2024-04-07 22:44:41 +0800

    Update Reward Func

--- custom_env/rllib_env_continous.py
+++ custom_env/rllib_env_continous.py
@@ -422,11 +422,12 @@ class RllibAnalogDesignAutoEnv(MultiAgentEnv):
 
             # Calculate reward
             rew = {a: -10 for a in self.agents}
-            if fail_tage:
-                rew_single = -10
-                logging.warning(f"Step!!!Reward is given to min due to some simulation failed")
-            else:
-                rew_single = cal_reward(self.ideal_specs, sim_result, self.norm_specs)
+            # if fail_tage:
+            #     rew_single = -10
+            #     logging.warning(f"Step!!!Reward is given to min due to some simulation failed")
+            # else:
+            #     rew_single = cal_reward(self.ideal_specs, sim_result, self.norm_specs)
+            rew_single = cal_reward(self.ideal_specs, sim_result, self.norm_specs)
             for agent_name in rew:
                 rew[agent_name] = rew_single
             logging.info(f"Step!!!Reward result: {rew_single} with step number: {self.step_num}")
