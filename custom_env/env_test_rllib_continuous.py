a5c661c0e49b8328b923c22382f518aa6f8014ae
HAN WU <whxhjs@gmail.com>
2024-04-07 23:36:01 +0800

    Update Reward Func

--- custom_env/env_test_rllib_continuous.py
+++ custom_env/env_test_rllib_continuous.py
@@ -3,7 +3,7 @@ from rllib_env_continous import RllibAnalogDesignAutoEnv
 
 if __name__ == "__main__":
     env = RllibAnalogDesignAutoEnv(generalize=True, specs_folder_name='sampled_specs',
-                                   config_folder_name='config', run_folder_name='run_test',
+                                   config_folder_name='config_is_continuous', run_folder_name='run_test',
                                    sim_output=False, init_method='file', action_mask=True, dc_check=True)
 
     check_env(env)
