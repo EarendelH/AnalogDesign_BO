9c9d9e81fe148bf47532e317b4fed61d39a2f0b9
HAN WU <whxhjs@gmail.com>
2024-04-09 23:09:22 +0800

    Debug

--- custom_env/env_test_rllib.py
+++ custom_env/env_test_rllib.py
@@ -2,8 +2,8 @@ from ray.rllib.utils import check_env
 from rllib_env import RllibAnalogDesignAutoEnv
 
 if __name__ == "__main__":
-    env = RllibAnalogDesignAutoEnv(generalize=True, specs_folder_name='sampled_specs',
-                                   config_folder_name='config', run_folder_name='run_test', sim_output=False,
+    env = RllibAnalogDesignAutoEnv(generalize=True, specs_folder_name='sampled_specs_ideal_source',
+                                   config_folder_name='config_ideal_source', run_folder_name='run_test', sim_output=False,
                                    init_method='file', action_mask=True, dc_check=True)
 
     check_env(env)
