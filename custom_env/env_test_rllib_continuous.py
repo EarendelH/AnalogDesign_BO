6ac47f897c24523f656aa02350763b1c5082550e
HAN WU <whxhjs@gmail.com>
2024-04-26 15:38:28 +0800

    Setup folder for JSSC10 Paper

--- custom_env/env_test_rllib_continuous.py
+++ custom_env/env_test_rllib_continuous.py
@@ -4,7 +4,7 @@ from rllib_env_continous import RllibAnalogDesignAutoEnv
 if __name__ == "__main__":
     env = RllibAnalogDesignAutoEnv(generalize=True, specs_folder_name='sampled_specs',
                                    netlist_folder_name='netlist_template', config_folder_name='config',
-                                   run_folder_name='run_test', sim_output=False, init_method='file',
+                                   run_folder_name='run_test', sim_output=False, init_method='half',
                                    action_mask=True, dc_check=True)
 
     check_env(env)
