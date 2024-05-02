e4188498aa5afdeb83d782f4053cc4cf8cb2ced1
HAN WU <whxhjs@gmail.com>
2024-05-02 22:28:59 +0800

    New Config

--- custom_env/env_test_rllib_matched.py
+++ custom_env/env_test_rllib_matched.py
@@ -3,7 +3,7 @@ from rllib_env import RllibAnalogDesignAutoEnv
 
 if __name__ == "__main__":
     env = RllibAnalogDesignAutoEnv(generalize=True, specs_folder_name='sampled_specs_Yan',
-                                   netlist_folder_name='netlist_template_Yan', config_folder_name='Yan',
+                                   netlist_folder_name='netlist_template_Yan', config_folder_name='config_Yan',
                                    run_folder_name='run_test', sim_output=False, init_method='half',
                                    dc_check=True)
 
