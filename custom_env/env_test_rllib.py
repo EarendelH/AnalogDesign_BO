dbecf1c79cb430b3ce8daa7dd26c4218990c4f27
HAN WU <whxhjs@gmail.com>
2024-05-06 20:38:22 +0800

    New Config

--- custom_env/env_test_rllib.py
+++ custom_env/env_test_rllib.py
@@ -2,9 +2,9 @@ from ray.rllib.utils import check_env
 from rllib_env import RllibAnalogDesignAutoEnv
 
 if __name__ == "__main__":
-    env = RllibAnalogDesignAutoEnv(generalize=True, specs_folder_name='sampled_specs',
-                                   netlist_folder_name='netlist_template', config_folder_name='config_Mohamed_JSSC10',
-                                   run_folder_name='run_test', sim_output=False, init_method='half',
+    env = RllibAnalogDesignAutoEnv(generalize=True, specs_folder_name='sampled_specs_Mohamed',
+                                   netlist_folder_name='netlist_template_Mohamed', config_folder_name='config_Mohamed',
+                                   run_folder_name='run_test', sim_output=False, init_method='file',
                                    dc_check=True)
 
     check_env(env)
