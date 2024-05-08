74a2812afa08ab9ec4f7598ca67117f4e5b1c7a7
HAN WU <whxhjs@gmail.com>
2024-05-08 10:50:34 +0800

    New Config

--- custom_env/util/findTrans.py
+++ custom_env/util/findTrans.py
@@ -228,7 +228,7 @@ def findShoot(filename):
     if stable_high_load_voltage >= stable_voltage * 1.1 or stable_high_load_voltage <= stable_voltage * 0.9:
         print("Warning! This LDO cannot be regulated to VREF under high load.")
         overshoot = 100.0
-        undershoot = 1.0
+        undershoot = 100.0
     if stable_light_load_voltage >= stable_voltage * 1.1 or stable_light_load_voltage <= stable_voltage * 0.9:
         print("Warning! This LDO cannot be regulated to VREF under light load.")
         overshoot = 100.0
@@ -287,7 +287,7 @@ def findShoot_Jiangping(filename):
     if stable_high_load_voltage >= stable_voltage * 1.1 or stable_high_load_voltage <= stable_voltage * 0.9:
         print("Warning! This LDO cannot be regulated to VREF under high load.")
         overshoot = 100.0
-        undershoot = 1.0
+        undershoot = 100.0
     if stable_light_load_voltage >= stable_voltage * 1.1 or stable_light_load_voltage <= stable_voltage * 0.9:
         print("Warning! This LDO cannot be regulated to VREF under light load.")
         overshoot = 100.0
@@ -339,7 +339,7 @@ def findShoot_Line_Reg(filename):
     if stable_high_load_voltage >= stable_voltage * 1.1 or stable_high_load_voltage <= stable_voltage * 0.9:
         print("Warning! This LDO cannot be regulated to VREF under high load.")
         overshoot = 100.0
-        undershoot = 1.0
+        undershoot = 100.0
     if stable_light_load_voltage >= stable_voltage * 1.1 or stable_light_load_voltage <= stable_voltage * 0.9:
         print("Warning! This LDO cannot be regulated to VREF under light load.")
         overshoot = 100.0
