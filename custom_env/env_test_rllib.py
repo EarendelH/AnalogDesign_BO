60f5a961604c89b4cf86123e99ff8e3ba316d6e9
HAN WU <whxhjs@gmail.com>
2024-05-14 20:23:35 +0800

    New Config

--- custom_env/env_test_rllib.py
+++ custom_env/env_test_rllib.py
@@ -2,10 +2,10 @@ from ray.rllib.utils import check_env
 from rllib_env import RllibAnalogDesignAutoEnv
 
 if __name__ == "__main__":
-    env = RllibAnalogDesignAutoEnv(generalize=True, specs_folder_name='sampled_specs_Yan',
-                                   netlist_folder_name='netlist_template_Yan', config_folder_name='config_Yan',
-                                   run_folder_name='run_test', sim_output=False, init_method='mixed',
-                                   dc_check=True)
+    env = RllibAnalogDesignAutoEnv(generalize=True, specs_folder_name='sampled_specs_Jianping',
+                                   netlist_folder_name='netlist_template_Jianping',
+                                   config_folder_name='config_Jianping',run_folder_name='run_test', sim_output=False
+                                   , init_method='mixed', dc_check=True)
 
     check_env(env)
     print("Test passed")
