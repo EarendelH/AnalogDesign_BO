6cd1bc898e4e56a757a489cc9ee8d499f39973be
HAN WU <whxhjs@gmail.com>
2024-05-15 16:27:43 +0800

    Debug: Apply deepcopy for dict

--- custom_env/env_test_rllib.py
+++ custom_env/env_test_rllib.py
@@ -2,9 +2,9 @@ from ray.rllib.utils import check_env
 from rllib_env import RllibAnalogDesignAutoEnv
 
 if __name__ == "__main__":
-    env = RllibAnalogDesignAutoEnv(generalize=True, specs_folder_name='sampled_specs_Jianping',
-                                   netlist_folder_name='netlist_template_Jianping',
-                                   config_folder_name='config_Jianping', run_folder_name='run_test', sim_output=False
+    env = RllibAnalogDesignAutoEnv(generalize=True, specs_folder_name='sampled_specs_Debashis',
+                                   netlist_folder_name='netlist_template_Debashis',
+                                   config_folder_name='config_Debashis', run_folder_name='run_test', sim_output=False
                                    , init_method='mixed', dc_check=True)
 
     check_env(env)
