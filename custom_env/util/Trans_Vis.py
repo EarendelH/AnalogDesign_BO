3acf9e4fb07a7dc558cd70d89c8330b84071798f
HAN WU <whxhjs@gmail.com>
2024-05-21 14:29:26 +0800

    New Process

--- custom_env/util/Trans_Vis.py
+++ custom_env/util/Trans_Vis.py
@@ -9,7 +9,7 @@ def scan_and_process(root_dir, highlight_subdir):
     results = {}
 
     for subdir in next(os.walk(root_dir))[1]:
-        target_file = os.path.join(root_dir, subdir, 'Trans_0_75V.raw', 'tran.tran.tran.encode')
+        target_file = os.path.join(root_dir, subdir, 'Trans_1_2V.raw', 'tran.tran.tran.encode')
         if os.path.isfile(target_file):
             trace_data = extractTransTrace(target_file)
             results[subdir] = {
