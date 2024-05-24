71724022b68ef528120683f5ac94d6569b4161f3
HAN WU <whxhjs@gmail.com>
2024-05-24 17:09:49 +0800

    Update init point

--- custom_env/debug_analyze_util/delete_old_file.py
+++ custom_env/debug_analyze_util/delete_old_file.py
@@ -1,25 +1,27 @@
 import os
 import shutil
 import time
+import concurrent.futures
 
-def delete_old_files_and_dirs(path):
 
+def delete_old_file_or_dir(path):
     current_time = time.time()
+    if os.path.isfile(path):
+        file_time = os.path.getmtime(path)
+        if current_time - file_time > 12 * 60 * 60:
+            os.remove(path)
+            print(f"Deleted file: {path}")
+    elif os.path.isdir(path):
+        dir_time = os.path.getmtime(path)
+        if current_time - dir_time > 12 * 60 * 60:
+            shutil.rmtree(path)
+            print(f"Deleted directory: {path}")
 
-    for root_dir, dirs, files in os.walk(path, topdown=False):
-        for name in files:
-            file_path = os.path.join(root_dir, name)
-            file_time = os.path.getmtime(file_path)
-            if current_time - file_time > 12 * 60 * 60:
-                os.remove(file_path)
-                print(f"Deleted file: {file_path}")
 
-        for name in dirs:
-            dir_path = os.path.join(root_dir, name)
-            dir_time = os.path.getmtime(dir_path)
-            if current_time - dir_time > 12 * 60 * 60:
-                shutil.rmtree(dir_path)
-                print(f"Deleted directory: {dir_path}")
+def delete_old_files_and_dirs(base_path):
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        for root_dir, dirs, files in os.walk(base_path, topdown=False):
+            executor.map(delete_old_file_or_dir, [os.path.join(root_dir, name) for name in files + dirs])
 
 
 delete_old_files_and_dirs(input("Please input the path: "))
