6e0243dd057d63f302fbb1495f709b47edfb11d9
HAN WU <whxhjs@gmail.com>
2024-05-28 23:16:17 +0800

    Update init point

--- custom_env/util/vis_trans.py
+++ custom_env/util/vis_trans.py
@@ -9,7 +9,7 @@ def scan_and_process(root_dir, highlight_subdir):
     results = {}
 
     for subdir in next(os.walk(root_dir))[1]:
-        target_file = os.path.join(root_dir, subdir, 'Trans_Line_Reg.raw', 'tran.tran.tran.encode')
+        target_file = os.path.join(root_dir, subdir, 'Trans_1_2V.raw', 'tran.tran.tran.encode')
         if os.path.isfile(target_file):
             trace_data = extractTransTrace(target_file)
             results[subdir] = {
@@ -43,8 +43,8 @@ def plot_data(results, highlight_subdir, root_dir):
     print(f"Image save to：{plt_path}")
 
 
-root_dir = '/Users/hanwu/Downloads/Log_N65/Jianping_C/5_28_eex02/select_point'
-highlight_subdir = 'tmp_20240526052211547943627'
+root_dir = '/Users/hanwu/Downloads/Log_N65/Jianping/5_28_UM/select_point'
+highlight_subdir = 'tmp_20240528120022477035927'
 scan_and_process(root_dir, highlight_subdir)
 
 
