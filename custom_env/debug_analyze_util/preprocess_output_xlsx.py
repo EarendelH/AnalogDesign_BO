56d1f32b8f9eb3c97a068f84ac098a6a9cf89065
HAN WU <whxhjs@gmail.com>
2024-06-03 21:42:18 +0800

    Update debug tool

--- custom_env/debug_analyze_util/preprocess_output_xlsx.py
+++ custom_env/debug_analyze_util/preprocess_output_xlsx.py
@@ -21,7 +21,7 @@ def process_excel(filepath):
     df = df.iloc[:, 2:]
 
     # Convert values with metric suffixes to float
-    for col in df.columns[22:]:
+    for col in df.columns[21:]:
         df[col] = df[col].apply(convert_to_float)
 
     # Find all columns that start with 'nf_'
