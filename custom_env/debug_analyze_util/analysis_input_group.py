952c8b64e9b31265461c32c7251b6d673954ef62
HAN WU <whxhjs@gmail.com>
2024-06-03 21:23:30 +0800

    Update debug tool

--- custom_env/debug_analyze_util/analysis_input_group.py
+++ custom_env/debug_analyze_util/analysis_input_group.py
@@ -76,7 +76,7 @@ plot_file_path = os.path.join(excel_dir, 'feature_clustering.png')
 plt.savefig(plot_file_path)
 
 from scipy.cluster.hierarchy import fcluster
-max_distance = 0.5
+max_distance = 0.1
 clusters = fcluster(Z, max_distance, criterion='distance')
 
 feature_groups = {}
