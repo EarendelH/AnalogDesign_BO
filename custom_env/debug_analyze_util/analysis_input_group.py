2d801a0d723a37011539aba76df3fc5b5723a58c
HAN WU <whxhjs@gmail.com>
2024-06-03 22:10:39 +0800

    Update debug tool

--- custom_env/debug_analyze_util/analysis_input_group.py
+++ custom_env/debug_analyze_util/analysis_input_group.py
@@ -134,7 +134,7 @@ plt.savefig(plot_file_path)
 
 # Group features based on the clustering results
 from scipy.cluster.hierarchy import fcluster
-max_distance = 0.1
+max_distance = 0.2
 clusters = fcluster(Z, max_distance, criterion='distance')
 
 feature_groups = {}
