d2dd7cd10991061034b00f09c1c590dafde489b5
HAN WU <whxhjs@gmail.com>
2024-06-04 22:48:23 +0800

    Update debug tool

--- custom_env/debug_analyze_util/vis_formated_xls.py
+++ custom_env/debug_analyze_util/vis_formated_xls.py
@@ -40,7 +40,7 @@ def plot_data(file_path, column_name):
 
 
 # Example usage
-file_path = '/Users/hanwu/Downloads/Log_N65/Jianping_C/6_1_UM/output_Jianping_UM_N65_C_format.xlsx'
+file_path = input('Enter the path to the processed Excel file: ')
 column_name_list = ['Reward', 'DC_IQ', 'Load_Reg_loadReg', 'Line_Reg_100m_lineReg', 'Line_Reg_1m_lineReg',
                     'Trans_1_2V_overShoot', 'Trans_1_2V_underShoot', 'Trans_0_75V_overShoot', 'Trans_0_75V_underShoot',
                     'Trans_Line_Reg_overShoot', 'Trans_Line_Reg_underShoot', 'PSR_psr_100', 'PSR_psr_1k', 'PSR_psr_10k',
