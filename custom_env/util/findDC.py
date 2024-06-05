6c2621e5735c74b883b005d445b4f94e31870faf
HAN WU <whxhjs@gmail.com>
2024-06-05 14:10:58 +0800

    New topology

--- custom_env/util/findDC.py
+++ custom_env/util/findDC.py
@@ -87,7 +87,7 @@ def findIQ_FC_SSF(filepath):
         print(f"Warning: {e}. Setting IQ to default (100.0).")
         value = 100.0
 
-    if value > 0.00005:
+    if value > 0.001:
         print(f"Power is too large, return to max 100.0 value")
         value = 100.0
     return {"IQ": value}
