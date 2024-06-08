cffa8abd6c50c4e022fe302f5ccabebb20fc93fa
HAN WU <whxhjs@gmail.com>
2024-06-08 21:44:05 +0800

    Update netlist

--- custom_env/env_test_rllib_continuous.py
+++ custom_env/env_test_rllib_continuous.py
@@ -2,9 +2,9 @@ from ray.rllib.utils import check_env
 from rllib_env_continous import RllibAnalogDesignAutoEnv
 
 if __name__ == "__main__":
-    env = RllibAnalogDesignAutoEnv(generalize=True, specs_folder_name='sampled_specs_Jianping_N65',
-                                   netlist_folder_name='netlist_template_Jianping_N65_bk',
-                                   config_folder_name='config_Jianping_N65_C',
+    env = RllibAnalogDesignAutoEnv(generalize=True, specs_folder_name='sampled_specs_Lab_N65',
+                                   netlist_folder_name='netlist_template_Lab_N65',
+                                   config_folder_name='config_Lab_N65',
                                    run_folder_name='run_test', sim_output=False, init_method='half',
                                    dc_check=True)
 
