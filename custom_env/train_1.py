0fd31dbdd8bf107c6df0e127d6a02247c0dc813c
HAN WU <whxhjs@gmail.com>
2024-06-18 20:40:22 +0800

    Multi-layer reinforcement learnning

---
+++ custom_env/train_1.py
@@ -0,0 +1,174 @@
+import subprocess
+import sys
+import os
+import argparse
+import yaml
+import torch
+
+import ray
+from ray import tune
+from ray.rllib.algorithms.ppo import PPOConfig
+from ray.tune.registry import register_env
+
+from rllib_env import RllibAnalogDesignAutoEnv
+
+
+def get_user_input(prompt, default_value):
+    """Collects user input or uses the default value if input is empty."""
+    user_input = input(f"{prompt} (Default: {default_value}): ").strip()
+    return user_input if user_input else default_value
+
+
+def confirm_settings(settings):
+    """Displays settings for user confirmation before proceeding with training."""
+    print("\nHere are your configuration settings:")
+    for k, v in settings.items():
+        print(f"{k}: {v}")
+    confirm = input("\nConfirm to start training? (y/n): ").lower().strip()
+    return confirm == "y"
+
+
+def main():
+    """Main function to run the script."""
+    settings = {}
+    confirm_flag = None
+    cpu_count = os.cpu_count()
+    gpu_count = torch.cuda.device_count()
+
+    parser = argparse.ArgumentParser(description="Train Analog Design AutoRL Environment")
+    parser.add_argument('--config_mode', type=str, choices=['interactive', 'file'], default='interactive',
+                        help="Mode for configuration settings")
+    parser.add_argument('--config_file', type=str, help="Path to configuration file")
+    args = parser.parse_args()
+
+    print("Config Mode: ", args.config_mode)
+
+    if args.config_mode == 'file':
+        if args.config_file is None:
+            print("Configuration file not provided. Exiting.")
+            sys.exit(1)
+        with open(args.config_file, 'r') as file:
+            settings = yaml.safe_load(file)
+        confirm_flag = True
+    if args.config_mode == 'interactive':
+        settings = {
+            "cpu_usage": get_user_input(f"Enter use CPU num, total available CPU is {cpu_count}"
+                                                   , "10"),
+            "gpu_usage": get_user_input(f"Enter use GPU num, total available CPU is {gpu_count}", "0"),
+            "generalize": get_user_input("Enable generalization (True/False)", "True"),
+            "netlist_folder_name": get_user_input("Name of netlist folder", "netlist_template"),
+            "specs_folder_name": get_user_input("Name of specs folder", "sampled_specs"),
+            "config_folder_name": get_user_input("Name of config folder", "config"),
+            "run_folder_name": get_user_input("Name of run folder", "run_test"),
+            "sim_output": get_user_input("Enable simulation output (True/False)", "False"),
+            "init_method": get_user_input("Initialization method (file/half/random/mixed)", "file"),
+            "dc_check": get_user_input("Enable step DC check (True/False)", "True"),
+            "restore_checkpoint": get_user_input("Restore from checkpoint? (True/False)", "False"),
+            "checkpoint_path": None,  # To be conditionally updated
+            "train_iterations": get_user_input("Train iterations(Default: 200)", "200"),
+        }
+
+        if settings["restore_checkpoint"]:
+            settings["checkpoint_path"] = get_user_input("Checkpoint path", "")
+        confirm_flag = confirm_settings(settings)
+
+    print(f"Configuration settings: {settings}")
+
+    num_cpu = int(settings["cpu_usage"])
+    num_gpu = int(settings["gpu_usage"])
+
+    settings["train_iterations"] = int(settings["train_iterations"])
+
+    if confirm_flag:
+        if args.config_mode == 'interactive':
+            # Convert string boolean values to Python boolean values
+            settings["generalize"] = settings["generalize"] == "True"
+            settings["sim_output"] = settings["sim_output"] == "True"
+            settings["dc_check"] = settings["dc_check"] == "True"
+            settings["restore_checkpoint"] = settings["restore_checkpoint"] == "True"
+
+        env_settings = {
+            "generalize": settings["generalize"],
+            "netlist_folder_name": settings["netlist_folder_name"],
+            "specs_folder_name": settings["specs_folder_name"],
+            "config_folder_name": settings["config_folder_name"],
+            "run_folder_name": settings["run_folder_name"],
+            "sim_output": settings["sim_output"],
+            "init_method": settings["init_method"],
+            "dc_check": settings["dc_check"],
+        }
+
+        # Environment initialization
+        def env_creator(_):
+            return RllibAnalogDesignAutoEnv(**env_settings)
+
+        register_env("AnalogDesignEnv_v0", env_creator)
+
+        # Configuration and launching of the training process would go here
+        # Similar to the previously described code for setting up and running the training
+
+        env_name = "AnalogDesignEnv_v0"
+        ray.init()
+
+        # Restore or Initialize train
+        restore_checkpoint = settings["restore_checkpoint"]
+        checkpoint_path = None
+        if restore_checkpoint == "True":
+            checkpoint_path = settings["checkpoint_path"]
+            assert os.path.exists(checkpoint_path), "Checkpoint path does not exist"
+            print(f"Restoring from checkpoint: {checkpoint_path}")
+
+        # Typing Train Iterations
+        train_iterations = settings["train_iterations"]
+
+        print("Starting training process...")
+
+        config = (
+            PPOConfig()
+            .environment(env="AnalogDesignEnv_v0", clip_actions=True)
+            .rollouts(num_rollout_workers=num_cpu)
+            .training(
+                train_batch_size=512,
+                lr=2e-4,
+                gamma=0.96,
+                lambda_=0.95,
+                use_gae=True,
+                clip_param=0.3,
+                grad_clip=None,
+                entropy_coeff=0.01,
+                vf_loss_coeff=0.25,
+                sgd_minibatch_size=64,
+                num_sgd_iter=24,
+                model={
+                    "fcnet_hiddens": [256, 256, 256, 256, 256],
+                }
+            )
+            .debugging(log_level="DEBUG")
+            .framework("torch")
+            .resources(num_gpus=num_gpu)
+            .multi_agent(
+                policies={"policy_1"},
+                policy_mapping_fn=(lambda aid, episode, worker, **kw: f"policy_{aid[-1]}"),
+                policies_to_train=["policy_1"],
+            )
+        )
+
+        user_home_dir = os.path.expanduser("~")
+
+        tune.run(
+            "PPO",
+            name="PPO",
+            stop={"training_iteration": train_iterations},
+            restore=checkpoint_path if restore_checkpoint == "True" else None,
+            checkpoint_freq=25,
+            checkpoint_at_end=True,
+            local_dir=f"{user_home_dir}/ray_results/{env_name}",
+            config=config.to_dict(),
+        )
+
+    else:
+        print("Configuration not confirmed. Training aborted.")
+
+
+if __name__ == "__main__":
+    main()
--- custom_env/util/findDC.py
+++ custom_env/util/findDC.py
@@ -88,3 +88,20 @@ def findIQ_KNL(filepath):
         value = 100.0
 
     return {"IQ": value}
+
+
+def findIQ_Jianping_DC_only(filepath):
+    search_keyword = "V2:p"
+    try:
+        value = extract_dcOP_data(filepath, search_keyword)
+        value = value - 0.05
+    except Exception as e:
+        print(f"Warning: {e}. return to max value.")
+        value = 100.0
+    if value > 0.00002:
+        print(f"Power is too large, return to max value")
+        value = 100.0
+    if value <= 0.0:
+        print(f"Power is lower than zero, impossible, return to max value")
+        value = 100.0
+    return {"IQ": value}
