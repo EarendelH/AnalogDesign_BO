e94b5a4acff4381b31cdb01325ab79c5c83d7ba2
HAN WU <whxhjs@gmail.com>
2024-06-25 16:54:37 +0800

    Add New Init method

--- custom_env/env_test_rllib_continuous.py
+++ custom_env/env_test_rllib_continuous.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     env = RllibAnalogDesignAutoEnv(generalize=True, specs_folder_name='sampled_specs_Lab_N65',
                                    netlist_folder_name='netlist_template_Lab_N65',
                                    config_folder_name='config_Lab_N65',
-                                   run_folder_name='run_test', sim_output=False, init_method='random',
+                                   run_folder_name='run_test', sim_output=False, init_method='file',
                                    dc_check=True, log_level='DEBUG')
 
     check_env(env)
