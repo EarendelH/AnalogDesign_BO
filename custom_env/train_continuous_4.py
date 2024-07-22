237c72a41c4a99a77a6c7013236ce69f5bbe0c9a
HAN WU <whxhjs@gmail.com>
2024-07-22 23:38:26 +0800

    change self.max_step

--- custom_env/train_continuous_4.py
+++ custom_env/train_continuous_4.py
@@ -140,7 +140,7 @@ def main():
                 sgd_minibatch_size=64,
                 num_sgd_iter=24,
                 model={
-                    "fcnet_hiddens": [256, 256, 256, 256, 256],
+                    "fcnet_hiddens": [256, 256, 256],
                 }
             )
             .debugging(log_level="DEBUG")
