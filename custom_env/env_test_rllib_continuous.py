caad0a2478cd0424afb080a005e1c2c3ffd70067
HAN WU <whxhjs@gmail.com>
2024-07-23 17:41:31 +0800

    Clean up

--- custom_env/env_test_rllib_continuous.py
+++ custom_env/env_test_rllib_continuous.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
                                    netlist_folder_name='netlist_template_Lab_N65_Yelab',
                                    config_folder_name='config_Lab_N65',
                                    run_folder_name='run_test', sim_output=False, init_method='half',
-                                   dc_check=True, dynamic_queue=True, log_level='DEBUG')
+                                   dc_check=True, dynamic_queue=True, log_level='INFO')
 
     check_env(env)
     print("Test passed")
