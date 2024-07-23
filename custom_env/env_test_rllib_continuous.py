5477438c84ac91676f71c1ac2a93eaf773c3cb24
HAN WU <whxhjs@gmail.com>
2024-07-23 17:36:30 +0800

    Clean up

--- custom_env/env_test_rllib_continuous.py
+++ custom_env/env_test_rllib_continuous.py
@@ -4,7 +4,7 @@ from rllib_env_continous import RllibAnalogDesignAutoEnv
 if __name__ == "__main__":
     env = RllibAnalogDesignAutoEnv(generalize=True, specs_folder_name='sampled_specs_Lab_N65',
                                    netlist_folder_name='netlist_template_Lab_N65_Yelab',
-                                   config_folder_name='config_Lab_N65_Original',
+                                   config_folder_name='config_Jianping_N65_C',
                                    run_folder_name='run_test', sim_output=False, init_method='half',
                                    dc_check=True, dynamic_queue=True, log_level='DEBUG')
 
