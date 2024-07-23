4657a949c961055649149346672a30691c5655f1
HAN WU <whxhjs@gmail.com>
2024-07-23 17:06:19 +0800

    Clean up

--- custom_env/env_test_rllib_continuous.py
+++ custom_env/env_test_rllib_continuous.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     env = RllibAnalogDesignAutoEnv(generalize=True, specs_folder_name='sampled_specs_Lab_N65',
                                    netlist_folder_name='netlist_template_Lab_N65_Yelab',
                                    config_folder_name='config_Lab_N65_Original',
-                                   run_folder_name='run_test', sim_output=False, init_method='random',
+                                   run_folder_name='run_test', sim_output=False, init_method='half',
                                    dc_check=True, dynamic_queue=True, log_level='DEBUG')
 
     check_env(env)
