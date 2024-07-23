7f6fee82284f6d889adfaf7f5fea8f9aa490f13d
HAN WU <whxhjs@gmail.com>
2024-07-23 18:15:56 +0800

    Clean up

--- custom_env/env_test_rllib_continuous.py
+++ custom_env/env_test_rllib_continuous.py
@@ -3,7 +3,7 @@ from rllib_env_continous import RllibAnalogDesignAutoEnv
 
 if __name__ == "__main__":
     env = RllibAnalogDesignAutoEnv(generalize=True, specs_folder_name='sampled_specs_Lab_N65',
-                                   netlist_folder_name='netlist_template_Lab_N65_Yelab',
+                                   netlist_folder_name='netlist_template_Lab_N65',
                                    config_folder_name='config_Lab_N65',
                                    run_folder_name='run_test', sim_output=False, init_method='half',
                                    dc_check=True, dynamic_queue=True, log_level='INFO')
