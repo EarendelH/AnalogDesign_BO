3f7c39d0ab810c2c486832971faac71bcbdea752
HAN WU <whxhjs@gmail.com>
2024-08-06 14:49:33 +0800

    New topology

--- custom_env/env_test_rllib_continuous.py
+++ custom_env/env_test_rllib_continuous.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
                                    netlist_folder_name='netlist_template_Cai',
                                    config_folder_name='config_Cai_Single',
                                    run_folder_name='run_test', sim_output=False, init_method='random',
-                                   dc_check=False, dynamic_queue=False, log_level='INFO')
+                                   dc_check=False, dynamic_queue=False, log_level='DEBUG')
 
     check_env(env)
     print("Test passed")
