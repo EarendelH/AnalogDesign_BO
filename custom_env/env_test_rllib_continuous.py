e2ec529304c95861ee98e50ec734e5abc34994d7
HAN WU <whxhjs@gmail.com>
2024-08-06 14:42:03 +0800

    New topology

--- custom_env/env_test_rllib_continuous.py
+++ custom_env/env_test_rllib_continuous.py
@@ -2,10 +2,10 @@ from ray.rllib.utils import check_env
 from rllib_env_continous import RllibAnalogDesignAutoEnv
 
 if __name__ == "__main__":
-    env = RllibAnalogDesignAutoEnv(generalize=True, specs_folder_name='sampled_specs_Lab_N65',
-                                   netlist_folder_name='netlist_template_Lab_N65',
-                                   config_folder_name='config_Lab_N65',
-                                   run_folder_name='run_test', sim_output=False, init_method='half',
+    env = RllibAnalogDesignAutoEnv(generalize=True, specs_folder_name='sampled_specs_Cai',
+                                   netlist_folder_name='netlist_template_Cai',
+                                   config_folder_name='config_Cai_Single',
+                                   run_folder_name='run_test', sim_output=False, init_method='random',
                                    dc_check=False, dynamic_queue=False, log_level='INFO')
 
     check_env(env)
