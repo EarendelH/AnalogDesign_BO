edea988ee90db94b4ca7051c09b7494ef38dfb8a
HAN WU <whxhjs@gmail.com>
2024-08-06 17:16:20 +0800

    New topology

--- custom_env/env_test_rllib_continuous.py
+++ custom_env/env_test_rllib_continuous.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     env = RllibAnalogDesignAutoEnv(generalize=True, specs_folder_name='sampled_specs_Cai',
                                    netlist_folder_name='netlist_template_Cai',
                                    config_folder_name='config_Cai_Single',
-                                   run_folder_name='run_test', sim_output=False, init_method='random',
+                                   run_folder_name='run_test', sim_output=False, init_method='half',
                                    dc_check=False, dynamic_queue=False, log_level='DEBUG')
 
     check_env(env)
