cd6fbf4545fdb2709d01369a751318ee12edff52
HAN WU <whxhjs@gmail.com>
2024-08-12 19:23:51 +0800

    Add APPO Algorithm and Merged training scripts

--- custom_env/env_test_rllib_continuous.py
+++ custom_env/env_test_rllib_continuous.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     env = RllibAnalogDesignAutoEnv(generalize=True, specs_folder_name='sampled_specs_Cai',
                                    netlist_folder_name='netlist_template_Cai',
                                    config_folder_name='config_Cai_Single',
-                                   run_folder_name='run_test', sim_output=False, init_method='random',
-                                   dc_check=False, dynamic_queue=False, log_level='DEBUG', max_step=100)
+                                   run_folder_name='run_test', sim_output=False, init_method='file',
+                                   dc_check=False, dynamic_queue=False, log_level='INFO', max_step=10)
 
     check_env(env)
     print("Test passed")
