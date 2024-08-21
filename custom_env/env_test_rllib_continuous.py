6fae3e2ac0ff25369b71af2500117a580e15204a
HAN WU <whxhjs@gmail.com>
2024-08-21 14:04:12 +0800

    Buck topology

--- custom_env/env_test_rllib_continuous.py
+++ custom_env/env_test_rllib_continuous.py
@@ -2,11 +2,11 @@ from ray.rllib.utils import check_env
 from rllib_env_continous import RllibAnalogDesignAutoEnv
 
 if __name__ == "__main__":
-    env = RllibAnalogDesignAutoEnv(generalize=True, specs_folder_name='sampled_specs_Buck',
-                                   netlist_folder_name='netlist_template_Buck',
-                                   config_folder_name='config_Buck',
+    env = RllibAnalogDesignAutoEnv(generalize=True, specs_folder_name='sampled_specs_Cai',
+                                   netlist_folder_name='netlist_template_Cai_YeLab',
+                                   config_folder_name='config_Cai_Multi',
                                    run_folder_name='run_test', sim_output=False, init_method='random',
-                                   dc_check=False, region_extract=False, dynamic_queue=False, log_level='DEBUG',
+                                   dc_check=False, region_extract=True, dynamic_queue=False, log_level='DEBUG',
                                    max_step=10)
 
     check_env(env)
--- custom_env/rllib_env_continous.py
+++ custom_env/rllib_env_continous.py
@@ -161,6 +161,7 @@ class RllibAnalogDesignAutoEnv(MultiAgentEnv):
             self.observation_space = flatten_obs_space(gen_obs_space(self.sim_config_dict, self.param_range_dict,
                                                                      agent_assign_dict))
         logging.debug(f"observation_space: {self.observation_space}")
+        logging.debug(f"the size of initial observation_space: {self.observation_space.shape[0]}")
         self._action_space_in_preferred_format = True
         self.action_space = gen_masked_continuous_action_space(self.device_mask_dict, agent_assign_dict)
         logging.debug(f"action_space: {self.action_space}")
@@ -428,6 +429,8 @@ class RllibAnalogDesignAutoEnv(MultiAgentEnv):
 
             # Share all observations
             observations = {agent: observation for agent in self.agents}
+            for agent in self.agents:
+                logging.debug(f"the size of step observation for agent {agent}: {observations[agent].shape[0]}")
 
             # Calculate reward
             rew_single = cal_reward(self.ideal_specs, sim_result, self.norm_specs)
