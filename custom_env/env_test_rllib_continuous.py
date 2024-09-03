bc58a2758d1a975a0e58634c6d9ea0c05a067885
HAN WU <whxhjs@gmail.com>
2024-09-03 15:29:25 +0800

    New topology, add corner

--- custom_env/env_test_rllib_continuous.py
+++ custom_env/env_test_rllib_continuous.py
@@ -4,17 +4,17 @@ from rllib_env_continous import RllibAnalogDesignAutoEnv
 if __name__ == "__main__":
     env = RllibAnalogDesignAutoEnv({'generalize': True,
                                     'max_step': 10,
-                                    'netlist_folder_name': 'netlist_template_Buck',
-                                    'specs_folder_name': 'sampled_specs_Buck',
-                                    'config_folder_name': 'config_Buck',
+                                    'netlist_folder_name': 'netlist_template_Haoqiang',
+                                    'specs_folder_name': 'sampled_specs_Haoqiang',
+                                    'config_folder_name': 'config_Haoqiang',
                                     'run_folder_name': 'run_test',
                                     'sim_output': False,
-                                    'init_method': 'random',
+                                    'init_method': 'file',
                                     'dc_check': False,
-                                    'region_extract': False,
+                                    'region_extract': True,
                                     'dynamic_queue': False,
                                     'log_level': 'INFO',
-                                    'reward_func': 'cal_reward_buck'
+                                    'reward_func': 'cal_reward_LDO'
                                 })
 
     check_env(env)
