0dc1de3efd2901c588c289bcd082ceedaf73f83b
HAN WU <whxhjs@gmail.com>
2024-09-06 22:03:15 +0800

    Recover from old version

--- custom_env/debug_analyze_util/filitered_csv_format.py
+++ custom_env/debug_analyze_util/filitered_csv_format.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import ast
 import math
-from tqdm import tqdm
+
 
 def flatten_nested_dict(d):
     flattened_dict = {}
@@ -9,16 +9,18 @@ def flatten_nested_dict(d):
         flattened_dict.update(nested_dict)
     return flattened_dict
 
+
 def is_valid_entry(flattened_dict):
     for key, value in flattened_dict.items():
         if value == 100.0 or value == 0.0:
             return False
     return True
 
+
 def split_dataframe_to_excel(df, max_rows=1000000, output_prefix='output'):
     num_files = math.ceil(len(df) / max_rows)
 
-    for i in tqdm(range(num_files), desc="Saving Excel files"):
+    for i in range(num_files):
         start_idx = i * max_rows
         end_idx = min((i + 1) * max_rows, len(df))
 
@@ -28,15 +30,6 @@ def split_dataframe_to_excel(df, max_rows=1000000, output_prefix='output'):
         df_subset.to_excel(output_file, index=False)
         print(f"Saved {output_file}")
 
-def process_reward(reward):
-    if isinstance(reward, str):
-        try:
-            reward_dict = ast.literal_eval(reward)
-            if isinstance(reward_dict, dict):
-                return next(iter(reward_dict.values()))
-        except:
-            pass
-    return reward
 
 # Main process
 file_path = input('Enter the file path: ')
@@ -47,26 +40,16 @@ original_length = len(data)
 print(f'Original data length: {original_length}')
 
 # Flatten the nested dictionary and filter invalid entries for 'Specs'
-print("Processing 'Specs' column...")
-tqdm.pandas(desc="Flattening 'Specs'")
-data['Valid_Specs'] = data['Specs'].progress_apply(lambda x: flatten_nested_dict(ast.literal_eval(x)))
+data['Valid_Specs'] = data['Specs'].apply(lambda x: flatten_nested_dict(ast.literal_eval(x)))
 data = data[data['Valid_Specs'].apply(is_valid_entry)]
 
 # Convert the valid flattened dictionaries into DataFrame columns for 'Specs'
-specs_df = pd.DataFrame(data['Valid_Specs'].tolist())
+specs_df = data['Valid_Specs'].apply(pd.Series)
 
 # Expand 'Parameters' column directly into DataFrame columns
-print("Processing 'Parameters' column...")
-tqdm.pandas(desc="Expanding 'Parameters'")
-params_df = data['Parameters'].progress_apply(ast.literal_eval).apply(pd.Series)
-
-# Process the 'Reward' column
-print("Processing 'Reward' column...")
-tqdm.pandas(desc="Processing 'Reward'")
-data['Reward'] = data['Reward'].progress_apply(process_reward)
+params_df = data['Parameters'].apply(ast.literal_eval).apply(pd.Series)
 
 # Combine the new columns with the original DataFrame (excluding the original 'Specs' and 'Valid_Specs' columns)
-print("Combining processed data...")
 expanded_data = pd.concat([data.drop(columns=['Specs', 'Valid_Specs', 'Parameters']), specs_df, params_df], axis=1)
 
 # Generate new file path prefix
