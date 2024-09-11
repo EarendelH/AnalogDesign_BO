4c0fef7e76477023c6aea68b3a62a1863d39c710
HAN WU <whxhjs@gmail.com>
2024-09-11 14:14:57 +0800

    Simple Buck topology

--- custom_env/env_test_rllib_continuous.py
+++ custom_env/env_test_rllib_continuous.py
@@ -4,18 +4,18 @@ from rllib_env_continous import RllibAnalogDesignAutoEnv
 if __name__ == "__main__":
     env = RllibAnalogDesignAutoEnv({'generalize': True,
                                     'max_step': 10,
-                                    'netlist_folder_name': 'netlist_template_Haoqiang_UM',
-                                    'specs_folder_name': 'sampled_specs_Haoqiang',
-                                    'config_folder_name': 'config_Haoqiang',
+                                    'netlist_folder_name': 'netlist_template_Buck_Simple',
+                                    'specs_folder_name': 'sampled_specs_Buck',
+                                    'config_folder_name': 'config_Buck_Simple',
                                     'run_folder_name': 'run_test',
                                     'sim_output': False,
-                                    'init_method': 'file',
-                                    'corner_sim': True,
+                                    'init_method': 'random',
+                                    'corner_sim': False,
                                     'dc_check': False,
-                                    'region_extract': True,
+                                    'region_extract': False,
                                     'dynamic_queue': False,
                                     'log_level': 'INFO',
-                                    'reward_func': 'cal_reward_LDO'
+                                    'reward_func': 'cal_reward_buck'
                                 })
 
     check_env(env)
