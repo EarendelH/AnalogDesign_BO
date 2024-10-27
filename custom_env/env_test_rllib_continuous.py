97fde9af9e3679482508795bb265204ae5831b46
HAN WU <whxhjs@gmail.com>
2024-10-27 16:45:35 +0800

    New config

--- custom_env/env_test_rllib_continuous.py
+++ custom_env/env_test_rllib_continuous.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
                                     'init_method': 'random',
                                     'corner_sim': False,
                                     'dc_check': False,
-                                    'region_extract': True,
+                                    'region_extract': False,
                                     'dynamic_queue': False,
                                     'log_level': 'INFO',
                                     'reward_func': 'cal_reward_general',
