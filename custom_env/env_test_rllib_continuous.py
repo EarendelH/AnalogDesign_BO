0863124d5abe296178826458d67e1a4ce7ec9f63
HAN WU <whxhjs@gmail.com>
2024-10-27 16:42:36 +0800

    New config

--- custom_env/env_test_rllib_continuous.py
+++ custom_env/env_test_rllib_continuous.py
@@ -4,9 +4,9 @@ from rllib_env_continous import RllibAnalogDesignAutoEnv
 if __name__ == "__main__":
     env = RllibAnalogDesignAutoEnv({'generalize': True,
                                     'max_step': 10,
-                                    'netlist_folder_name': 'netlist_template_Haoqiang_SMIC55',
-                                    'specs_folder_name': 'sampled_specs_Haoqiang',
-                                    'config_folder_name': 'config_Haoqiang_Regroup',
+                                    'netlist_folder_name': 'netlist_template_OTA',
+                                    'specs_folder_name': 'sampled_specs_OTA',
+                                    'config_folder_name': 'config_OTA',
                                     'run_folder_name': 'run_test',
                                     'sim_output': False,
                                     'init_method': 'random',
@@ -15,7 +15,7 @@ if __name__ == "__main__":
                                     'region_extract': True,
                                     'dynamic_queue': False,
                                     'log_level': 'INFO',
-                                    'reward_func': 'cal_reward_Haoqiang',
+                                    'reward_func': 'cal_reward_general',
                                     'continue_steps_enable': False
                                 })
 
