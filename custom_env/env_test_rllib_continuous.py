670f17a17697f79e46e4d184fd4dc8ab6889c1ca
HAN WU <whxhjs@gmail.com>
2024-10-27 23:31:12 +0800

    New config

--- custom_env/env_test_rllib_continuous.py
+++ custom_env/env_test_rllib_continuous.py
@@ -3,10 +3,10 @@ from rllib_env_continous import RllibAnalogDesignAutoEnv
 
 if __name__ == "__main__":
     env = RllibAnalogDesignAutoEnv({'generalize': True,
-                                    'max_step': 10,
-                                    'netlist_folder_name': 'netlist_template_OTA',
-                                    'specs_folder_name': 'sampled_specs_OTA',
-                                    'config_folder_name': 'config_OTA',
+                                    'max_step': 1,
+                                    'netlist_folder_name': 'netlist_original_Cai',
+                                    'specs_folder_name': 'sampled_specs_Cai',
+                                    'config_folder_name': 'config_Cai_Multi',
                                     'run_folder_name': 'run_test',
                                     'sim_output': False,
                                     'init_method': 'random',
@@ -15,7 +15,7 @@ if __name__ == "__main__":
                                     'region_extract': False,
                                     'dynamic_queue': False,
                                     'log_level': 'INFO',
-                                    'reward_func': 'cal_reward_general',
+                                    'reward_func': 'cal_reward_LDO',
                                     'continue_steps_enable': False
                                 })
 
