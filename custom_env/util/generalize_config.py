6a3f0de57f357cc71e342de1c8c25223834150b4
HAN WU <whxhjs@gmail.com>
2024-10-28 22:26:06 +0800

    New config

--- custom_env/util/generalize_config.py
+++ custom_env/util/generalize_config.py
@@ -8,6 +8,7 @@ def generalize_config(generalize, path):
         specs_path_list = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
         select_specs_path = random.choice(specs_path_list)
         specs_config_path = os.path.join(path, select_specs_path)
+        print(f"Debug!!! Selecting specs config: {specs_config_path}")
     else:
         specs_config_path = path
 
