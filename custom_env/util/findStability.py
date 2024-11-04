f100259b74c3bc3917f474d11234722c51791782
HAN WU <whxhjs@gmail.com>
2024-11-04 14:32:30 +0800

    New config

--- custom_env/util/findStability.py
+++ custom_env/util/findStability.py
@@ -2,7 +2,7 @@ import re
 import os
 import subprocess
 import numpy as np
-from extract_bode import analyze_loop_gain
+from extract_bode import analyze_loop_gain_only_value
 # from util.extract_bode import analyze_loop_gain_only_value
 
 
