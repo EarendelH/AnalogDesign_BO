3dd0a5d19315227265805c5496824288ae5ddcf0
HAN WU <whxhjs@gmail.com>
2025-01-13 11:34:18 +0800

    Add new constrain type: range

--- custom_env/util/normlization.py
+++ custom_env/util/normlization.py
@@ -14,11 +14,11 @@ def norm_ideal_spec(input_specs, norm_specs):
         norm_cur_specs_sub = {}
         for sub_key, value in sub_dict.items():
             ideal_value = value
-            # print(f"Debug, cur_value = {ideal_value}")
+            print(f"Debug, cur_value = {ideal_value}")
             input_value = input_specs[sub_key]['value']
-            # print(f"Debug, ideal_value = {input_value}")
+            print(f"Debug, ideal_value = {input_value}")
             norm_value = (input_value - ideal_value) / (input_value + ideal_value)
-            # print(f"Debug, norm_value = {norm_value}")
+            print(f"Debug, norm_value = {norm_value}")
             norm_cur_specs_sub[sub_key] = norm_value
         norm_cur_specs[key] = norm_cur_specs_sub
 
