eca9dfa80802938083f490da13bd4c80d4ef542c
HAN WU <whxhjs@gmail.com>
2025-01-19 17:07:52 +0800

    new topology

--- custom_env/env_test_rllib_continuous.py
+++ custom_env/env_test_rllib_continuous.py
@@ -4,7 +4,7 @@ from rllib_env_continous import RllibAnalogDesignAutoEnv
 if __name__ == "__main__":
     env = RllibAnalogDesignAutoEnv({'generalize': True,
                                     'max_step': 1,
-                                    'netlist_folder_name': 'netlist_template_DRMOS',
+                                    'netlist_folder_name': 'netlist_original_AXS',
                                     'specs_folder_name': 'sampled_specs_DRMOS',
                                     'config_folder_name': 'config_DRMOS',
                                     'run_folder_name': 'run_test',
