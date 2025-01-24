6ed1e7c393246c3f92099695fb1c168875816c1a
HAN WU <whxhjs@gmail.com>
2025-01-24 09:27:58 +0800

    new topology

--- custom_env/env_test_rllib_continuous_psfascii.py
+++ custom_env/env_test_rllib_continuous_psfascii.py
@@ -4,7 +4,7 @@ from rllib_env_continous_psfascii import RllibAnalogDesignAutoEnv
 if __name__ == "__main__":
     env = RllibAnalogDesignAutoEnv({'generalize': True,
                                     'max_step': 1,
-                                    'netlist_folder_name': 'netlist_template_AXS',
+                                    'netlist_folder_name': 'netlist_template_AXS_UM',
                                     'specs_folder_name': 'sampled_specs_AXS',
                                     'config_folder_name': 'config_AXS',
                                     'run_folder_name': 'run_test',
