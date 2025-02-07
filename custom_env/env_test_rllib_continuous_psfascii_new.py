5a0b73ce06e5dfd5513003883790ca84724ae2ee
HAN WU <whxhjs@gmail.com>
2025-02-07 18:57:23 +0800

    new topology

---
+++ custom_env/env_test_rllib_continuous_psfascii_new.py
@@ -0,0 +1,23 @@
+from ray.rllib.utils.pre_checks.env import check_multiagent_environments
+from rllib_env_continous_psfascii import RllibAnalogDesignAutoEnv
+
+if __name__ == "__main__":
+    env = RllibAnalogDesignAutoEnv({'generalize': True,
+                                    'max_step': 1,
+                                    'netlist_folder_name': 'netlist_template_AXS',
+                                    'specs_folder_name': 'sampled_specs_AXS',
+                                    'config_folder_name': 'config_AXS_Simple',
+                                    'run_folder_name': 'run_test',
+                                    'sim_output': False,
+                                    'init_method': 'random',
+                                    'corner_sim': False,
+                                    'dc_check': False,
+                                    'region_extract': False,
+                                    'dynamic_queue': False,
+                                    'log_level': 'DEBUG',
+                                    'reward_func': 'cal_reward_AXS',
+                                    'continue_steps_enable': False
+                                })
+
+    check_multiagent_environments(env)
+    print("Test passed")
