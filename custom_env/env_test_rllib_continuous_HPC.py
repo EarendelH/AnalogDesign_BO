37408737abdf74860e385d5e0e981ba50d260da7
HAN WU <whxhjs@gmail.com>
2025-04-08 13:46:03 +0800

    New config

---
+++ custom_env/env_test_rllib_continuous_HPC.py
@@ -0,0 +1,24 @@
+from ray.rllib.utils import check_env
+from rllib_env_continous import RllibAnalogDesignAutoEnv
+
+if __name__ == "__main__":
+    env = RllibAnalogDesignAutoEnv({'generalize': True,
+                                    'max_step': 1,
+                                    'netlist_folder_name': 'netlist_template_Haoqiang_HPC',
+                                    'specs_folder_name': 'sampled_specs_Haoqiang',
+                                    'config_folder_name': 'config_Haoqiang_Regroup',
+                                    'run_folder_name': 'run_test',
+                                    'sim_output': False,
+                                    'init_method': 'file',
+                                    'corner_sim': False,
+                                    'dc_check': False,
+                                    'region_extract': False,
+                                    'dynamic_queue': False,
+                                    'log_level': 'DEBUG',
+                                    'reward_func': 'cal_reward_Haoqiang',
+                                    'continue_steps_enable': False,
+                                    'HPC': True
+                                })
+
+    check_env(env)
+    print("Test passed")
