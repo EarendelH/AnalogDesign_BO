1848c09d6f06abecb02682b98a7190a0b38329cf
HAN WU <whxhjs@gmail.com>
2025-04-09 11:44:58 +0800

    New config

--- custom_env/env_test_rllib_continuous_HPC.py
+++ custom_env/env_test_rllib_continuous_HPC.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
                                     'log_level': 'DEBUG',
                                     'reward_func': 'cal_reward_Haoqiang',
                                     'continue_steps_enable': False,
-                                    'HPC': False
+                                    'HPC': True
                                 })
 
     check_env(env)
