9ae9258b6c120e89741a92e9b37b4f0a80fefc8e
HAN WU <whxhjs@gmail.com>
2025-04-10 01:17:32 +0800

    debug

--- custom_env/train_continuous.py
+++ custom_env/train_continuous.py
@@ -298,7 +298,11 @@ def main():
                 config = (
                     PPOConfig()
                     .environment(env="AnalogDesignEnv_v0", clip_actions=True)
-                    .rollouts(num_rollout_workers=num_cpu)
+                    .rollouts(
+                        num_rollout_workers=num_cpu,
+                        # 添加rollout_fragment_length参数，确保其值大于0
+                        rollout_fragment_length=4
+                    )
                     .training(
                         # train_batch_size=512,
                         train_batch_size=16,
