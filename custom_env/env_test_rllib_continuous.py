bbf826e8a970a79bc363ea50923cb409a703d89c
HAN WU <whxhjs@gmail.com>
2025-04-19 16:29:48 +0800

    New config

--- custom_env/env_test_rllib_continuous.py
+++ custom_env/env_test_rllib_continuous.py
@@ -4,9 +4,9 @@ from rllib_env_continous import RllibAnalogDesignAutoEnv
 if __name__ == "__main__":
     env = RllibAnalogDesignAutoEnv({'generalize': True,
                                     'max_step': 1,
-                                    'netlist_folder_name': 'netlist_template_AXS',
-                                    'specs_folder_name': 'sampled_specs_AXS_no_Mismatch',
-                                    'config_folder_name': 'config_AXS_no_Mismatch',
+                                    'netlist_folder_name': 'netlist_template_Lab_N65',
+                                    'specs_folder_name': 'sampled_specs_Lab_N65',
+                                    'config_folder_name': 'config_Lab_N65',
                                     'run_folder_name': 'run_test',
                                     'sim_output': False,
                                     'init_method': 'file',
--- custom_env/env_test_rllib_continuous_psfascii.py
+++ custom_env/env_test_rllib_continuous_psfascii.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
                                     'dc_check': False,
                                     'region_extract': False,
                                     'dynamic_queue': False,
-                                    'log_level': 'DEBUG',
+                                    'log_level': 'INFO',
                                     'reward_func': 'cal_reward_AXS',
                                     'continue_steps_enable': False
                                 })
