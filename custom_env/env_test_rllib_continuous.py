d4bf30347c67fac41e1eca08f82484f362d56c2e
HAN WU <whxhjs@gmail.com>
2025-04-26 23:37:51 +0800

    New config

--- custom_env/env_test_rllib_continuous.py
+++ custom_env/env_test_rllib_continuous.py
@@ -4,9 +4,9 @@ from rllib_env_continous import RllibAnalogDesignAutoEnv
 if __name__ == "__main__":
     env = RllibAnalogDesignAutoEnv({'generalize': True,
                                     'max_step': 1,
-                                    'netlist_folder_name': 'netlist_template_Lab_N65',
-                                    'specs_folder_name': 'sampled_specs_Lab_N65',
-                                    'config_folder_name': 'config_Lab_N65',
+                                    'netlist_folder_name': 'netlist_template_Haoqiang',
+                                    'specs_folder_name': 'sampled_specs_Haoqiang',
+                                    'config_folder_name': 'config_Haoqiang_Regroup',
                                     'run_folder_name': 'run_test',
                                     'sim_output': False,
                                     'init_method': 'file',
@@ -15,7 +15,7 @@ if __name__ == "__main__":
                                     'region_extract': False,
                                     'dynamic_queue': False,
                                     'log_level': 'INFO',
-                                    'reward_func': 'cal_reward_LDO',
+                                    'reward_func': 'cal_reward_Haoqiang',
                                     'continue_steps_enable': False
                                 })
 
