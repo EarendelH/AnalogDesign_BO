0f4cb0ff2a610d45b98100b6177429939612150d
HAN WU <whxhjs@gmail.com>
2025-05-14 20:45:28 +0800

    Enhance signal processing by adding rescaling functionality and modifying length checks

--- custom_env/env_test_rllib_continuous_psfascii.py
+++ custom_env/env_test_rllib_continuous_psfascii.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
                                     'max_step': 1,
                                     'netlist_folder_name': 'netlist_template_AXS',
                                     'specs_folder_name': 'sampled_specs_AXS_no_Mismatch',
-                                    'config_folder_name': 'config_AXS_no_Mismatch',
+                                    'config_folder_name': 'config_AXS_AutoGroup_4',
                                     'run_folder_name': 'run_test',
                                     'sim_output': False,
                                     'init_method': 'file',
