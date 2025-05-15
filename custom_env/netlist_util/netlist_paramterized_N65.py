b86e747d7865614191d3d59173dd579411d738d7
HAN WU <whxhjs@gmail.com>
2025-05-15 17:40:07 +0800

    Enhance signal processing by adding rescaling functionality and modifying length checks

--- custom_env/netlist_util/netlist_paramterized_N65.py
+++ custom_env/netlist_util/netlist_paramterized_N65.py
@@ -296,8 +296,8 @@ def netlist_parameterized(input_scs_path, output_scs_path, output_yaml_path):
 
 
 if __name__ == "__main__":
-    input_scs_folder = "/Users/hanwu/ML/AnalogDesignAuto_MultiAgent/custom_env/netlist_original/netlist_original_Jianping_N65"
-    output_scs_folder = "/Users/hanwu/ML/AnalogDesignAuto_MultiAgent/custom_env/netlist_template/netlist_template_Jianping_N65"
+    input_scs_folder = "/Users/hanwu/ML/AnalogDesignAuto_MultiAgent/custom_env/netlist_original/netlist_original_LFM"
+    output_scs_folder = "/Users/hanwu/ML/AnalogDesignAuto_MultiAgent/custom_env/netlist_template/netlist_template_LFM"
     for file in os.listdir(input_scs_folder):
         if file.endswith(".scs"):
             input_scs = os.path.join(input_scs_folder, file)
