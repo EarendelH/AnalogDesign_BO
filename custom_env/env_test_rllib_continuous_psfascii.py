cbac6ce5e6d4fbc0d55c7a02d86bf5689d0a4443
HAN WU <whxhjs@gmail.com>
2025-05-21 15:17:50 +0800

    New Config

--- custom_env/env_test_rllib_continuous_psfascii.py
+++ custom_env/env_test_rllib_continuous_psfascii.py
@@ -4,9 +4,9 @@ from rllib_env_continous_psfascii import RllibAnalogDesignAutoEnv
 if __name__ == "__main__":
     env = RllibAnalogDesignAutoEnv({'generalize': True,
                                     'max_step': 1,
-                                    'netlist_folder_name': 'netlist_template_AXS_UM',
-                                    'specs_folder_name': 'sampled_specs_AXS_no_Mismatch',
-                                    'config_folder_name': 'config_AXS_AutoGroup_7',
+                                    'netlist_folder_name': 'netlist_template_LFM',
+                                    'specs_folder_name': 'sampled_specs_LFM',
+                                    'config_folder_name': 'config_LFM',
                                     'run_folder_name': 'run_test',
                                     'sim_output': False,
                                     'init_method': 'file',
@@ -15,7 +15,7 @@ if __name__ == "__main__":
                                     'region_extract': False,
                                     'dynamic_queue': False,
                                     'log_level': 'INFO',
-                                    'reward_func': 'cal_reward_AXS',
+                                    'reward_func': 'cal_reward_LFM',
                                     'continue_steps_enable': False
                                 })
 
