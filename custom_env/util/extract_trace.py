9ba288d6fdc752050322e1724e184848f90b289b
HAN WU <whxhjs@gmail.com>
2025-05-26 20:09:00 +0800

    Simple Config

--- custom_env/util/extract_trace.py
+++ custom_env/util/extract_trace.py
@@ -380,5 +380,5 @@ def generate_trans_signals_csv(file_path):
 
 # if __name__ == "__main__":
 #     # Example usage
-#     file_path = "/Users/hanwu/Downloads/Trans/LFM_Trans/Netlist_Select/Trans_10.raw/tran.tran.tran"
+#     file_path = "/Users/hanwu/Downloads/Trans/LFM_Trans/Netlist_Select/Trans_9.raw/tran.tran.tran"
 #     generate_trans_signals_csv(file_path)
