90b22747919c0afcdd5b11a6b851951e9bf767a8
HAN WU <whxhjs@gmail.com>
2025-05-27 16:49:44 +0800

    Simple Config

--- custom_env/env_test_rllib_continuous_psfascii.py
+++ custom_env/env_test_rllib_continuous_psfascii.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
                                     'dc_check': False,
                                     'region_extract': False,
                                     'dynamic_queue': False,
-                                    'log_level': 'DEUBG',
+                                    'log_level': 'DEBUG',
                                     'reward_func': 'cal_reward_LFM',
                                     'continue_steps_enable': False
                                 })
